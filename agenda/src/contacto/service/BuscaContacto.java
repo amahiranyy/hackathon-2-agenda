6b1c5b2c83a58860185373e148d6d9aef809154f
Karen Nava <by.anna.kn@gmail.com>
2025-11-03 15:31:38 -0600

    agrego excepcion y buscar contacto

--- agenda/src/contacto/service/BuscaContacto.java
+++ agenda/src/contacto/service/BuscaContacto.java
@@ -1,4 +1,21 @@
 package contacto.service;
 
+import contacto.exceptions.ExceptionContactoNoExiste;
+import contacto.model.Contacto;
+
+import java.util.List;
+
 public class BuscaContacto {
+    public Contacto buscar(List<Contacto> contactos, String nombre, String apellido) throws ExceptionContactoNoExiste {
+        String nombreBuscado = nombre == null ? "" : nombre.trim().toLowerCase() ;
+        String apellidoBuscado = apellido == null ? "" : apellido.trim().toLowerCase();
+        for (Contacto contactoActual : contactos){
+            String nombreActual = contactoActual.getNombre() == null ? "" : contactoActual.getNombre().trim().toLowerCase();
+            String apellidoActual = contactoActual.getApellido() == null ? "" : contactoActual.getApellido().trim().toLowerCase() ;
+            if  (nombreActual.equals(nombreBuscado) && apellidoActual.equals(apellidoBuscado)){
+                return contactoActual;
+            }
+        }
+        throw new ExceptionContactoNoExiste();
+    }
 }
