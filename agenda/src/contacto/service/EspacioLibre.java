2f80bae63aef854830636c8178d524a14504b7f3
ximena <ximegames@gmail.com>
2025-11-03 14:45:59 -0600

    Metodo EspacioLibre

--- agenda/src/contacto/service/EspacioLibre.java
+++ agenda/src/contacto/service/EspacioLibre.java
@@ -1,4 +1,14 @@
 package contacto.service;
 
+import contacto.model.Contacto;
+
 public class EspacioLibre {
+    ListarContactos listaContactos = new ListarContactos();
+
+    // Muestra cuántos contactos más se pueden agregar a la agenda.
+    public int espacioLibre(){
+        int maximo = 10;
+        int contactosExistentes = listaContactos.contacto.size(); // Se utiliza el método size() para obtener el número total de elementos
+        return maximo-contactosExistentes;
+    }
 }
