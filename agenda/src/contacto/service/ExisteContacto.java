c2f24a547783f71c443892f2ce9ea1837364e535
Ricardo Avilés Sánchez <edu_jesi123@hotmail.com>
2025-11-03 18:28:01 -0600

    Update ExisteContacto.java

--- agenda/src/contacto/service/ExisteContacto.java
+++ agenda/src/contacto/service/ExisteContacto.java
@@ -1,4 +1,19 @@
 package contacto.service;
 
+import contacto.model.Contacto;
+
 public class ExisteContacto {
+    public boolean existeContacto(String nombre, String apellido) {
+        ListarContactos listarContactos = new ListarContactos();
+        // Variable de verificación
+        boolean verificacion = false;
+        // Verificar si ya existe un contacto con el mismo nombre y apellido
+        for (Contacto c : listarContactos.contacto) {
+            if (c.getNombre().equalsIgnoreCase(nombre) && c.getApellido().equalsIgnoreCase(apellido)) {
+                System.out.println("⚠️ Ya existe un contacto con ese nombre y apellido.");
+                verificacion = true;
+            }
+        }
+        return verificacion;
+    }
 }
